7c62f1f98cc175435bea18bcd905578c2156e8fd
irawanria85 <irawanria85@gmail.com>
2019-05-21 00:56:37 +0700

    Create redirect10playvideo.js

---
+++ redirect10playvideo.js
@@ -0,0 +1,6 @@
+/* Redirect */
+function eventualRedirect(redirectTo, timeoutPeriod) {
+setTimeout("location.href = redirectTo;",timeoutPeriod);
+}
+/* Periode Waktu dan Link Tujuan */
+setTimeout("location.href = 'https://aridjaya.blogspot.com/p/play-video.html';",600000);
