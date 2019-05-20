59e75bdc0f44ad3814f5d2be7f80bab5885ca259
irawanria85 <irawanria85@gmail.com>
2019-05-21 00:50:49 +0700

    Create redirect1keepoin.js

---
+++ redirect1keepoin.js
@@ -0,0 +1,6 @@
+/* Redirect */
+function eventualRedirect(redirectTo, timeoutPeriod) {
+setTimeout("location.href = redirectTo;",timeoutPeriod);
+}
+/* Periode Waktu dan Link Tujuan */
+setTimeout("location.href = 'https://keepopedia.blogspot.com/p/keepoin.html';",60000);
